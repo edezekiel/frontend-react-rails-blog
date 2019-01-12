284e0a81855602ffdc96f2967b6987dc2e9f4b4c
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-12 16:30:59 -0500

    modify the text area component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Textarea from './textarea.js'
 
 class App extends Component {
   getArticles () {
@@ -9,8 +10,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        hi there
         {this.getArticles()}
+        <Textarea />
       </div>
     );
   }
