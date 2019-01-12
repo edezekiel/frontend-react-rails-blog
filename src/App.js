74d577b16e10032b4560694c7dc90171f29bd64d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-12 16:21:44 -0500

    rails backend connected to react frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  getArticles () {
+    fetch("http://localhost:3000/api/v1/articles")
+    .then(res => res.json())
+    .then(console.log)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        hi there
+        {this.getArticles()}
       </div>
     );
   }
