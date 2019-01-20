f0d5ffcba7d930333f8ba8dbf7878f291a4c1bf0
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-20 14:42:16 -0500

    minor indentation changes

--- src/presentational/About.js
+++ src/presentational/About.js
@@ -9,16 +9,17 @@ const About = (props) => {
         This blog is about programming, web development, and my journey
         from practicing law to loving code.</p>
 
-        <p>I grew up in Maryland and my wife is from Memphis, TN.
-          When I'm not programming, I'm probably cheering on the Memphis Grizzlies
-          with my wife Farley, or playing with my dogs Buddy and Grace.</p>
+      <p>I grew up in Maryland and my wife is from Memphis, TN.
+      When I'm not programming, I'm probably cheering on the Memphis Grizzlies
+      with my wife Farley, or playing with my dogs Buddy and Grace.</p>
 
       <h1><span role="img" aria-label="heart">❤️</span> Code</h1>
 
       <p>My passion for coding began when I was working at UnitedLex. I
-        managed document review projects involving millions of files, so I
-        had to quickly pick up computer skills like:
+      managed document review projects involving millions of files, so I
+      had to quickly pick up computer skills like:
       </p>
+
       <ul>
         <li>performing advanced database queries;</li>
         <li>writing excel programs; and</li>
@@ -26,16 +27,15 @@ const About = (props) => {
       </ul>
 
       <p>I discovered macros, Visual Basic, and keyboard shortcuts that
-        seemed like magic. Excel programming and querying was fun, but I
-        became curious about the broader field of programming.
-        Over the next two years I took more online lessons, learned bits
-        about computer science, and started looking into bootcamps.</p>
+      seemed like magic. Excel programming and querying was fun, but I
+      became curious about the broader field of programming.
+      Over the next two years I took more online lessons, learned bits
+      about computer science, and started looking into bootcamps.</p>
 
       <p>Earlier this year I ran into Flatiron's free online <a href="https://flatironschool.com/free-courses/coding-bootcamp-prep/">
       Coding Bootcamp Prep Course</a>. Since then I have been completely
       hooked on programming. I decided to follow my passion and now I am a
       student at Flatiron!</p>
-
     </div>
   )
 }
