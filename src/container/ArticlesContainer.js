4a5baefc61597ddcf6797427520aea02d715cadf
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-21 11:43:23 -0500

    restyle the articles page

--- src/container/ArticlesContainer.js
+++ src/container/ArticlesContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Header, List } from 'semantic-ui-react'
+import { Header, List, Image } from 'semantic-ui-react'
 import ArticleListItem from '../presentational/ArticleListItem.js'
 
 const URL = "http://localhost:3000/api/v1/articles"
@@ -17,11 +17,17 @@ export default class ArticlesContainer extends Component {
     return (
       <div>
       <Header size="huge">Articles</Header>
-        <List animated size="medium" relaxed>
+        <List animated divided size="medium" relaxed>
           {this.state.articles.map((article) =>
             <List.Item key={article.id}>
-              <ArticleListItem
-                article={article} />
+              <Image avatar floated='left' src='https://vignette.wikia.nocookie.net/gameofthrones/images/5/5c/Eddard_1x01.jpg/revision/latest?cb=20120511213934' />
+              <List.Content verticalAlign='middle' floated='left'>
+                <ArticleListItem
+                  article={article} />
+              </List.Content>
+              <List.Content verticalAlign='middle' floated='right'>
+                <Header as='h5'>January 19, 2019</Header>
+              </List.Content>
             </List.Item>
           )}
         </List>
