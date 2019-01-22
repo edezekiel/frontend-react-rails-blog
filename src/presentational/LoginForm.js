93211e71b47f1ef78294fe07d52658051b53e9c6
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-22 17:12:38 -0500

    additional new article form styling

--- src/presentational/LoginForm.js
+++ src/presentational/LoginForm.js
@@ -1,27 +1,29 @@
 import React from 'react'
-import { Button, Form } from 'semantic-ui-react'
+import { Button, Form, Container } from 'semantic-ui-react'
 
 const LoginForm = (props) => {
   return(
-    <Form onSubmit={props.handleSubmit}>
-      <Form.Field>
-        <label>Username
-          <input
-            type="text"
-            name="username"/>
-        </label>
-      </Form.Field>
-      <Form.Field>
-        <label>Password
-          <input
-            type="text"
-            name="password"/>
-        </label>
-      </Form.Field>
-      <Form.Field>
-        <Button primary type="submit">Submit</Button>
-      </Form.Field>
-    </Form>
+    <Container text textAlign="justified" style={{"margin-top": "7rem"}}>
+      <Form onSubmit={props.handleSubmit}>
+        <Form.Field>
+          <label>Username
+            <input
+              type="text"
+              name="username"/>
+          </label>
+        </Form.Field>
+        <Form.Field>
+          <label>Password
+            <input
+              type="text"
+              name="password"/>
+          </label>
+        </Form.Field>
+        <Form.Field>
+          <Button primary type="submit">Submit</Button>
+        </Form.Field>
+      </Form>
+    </Container>
   )
 }
 
