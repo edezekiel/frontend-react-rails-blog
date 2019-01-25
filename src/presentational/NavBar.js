05989432f8d7d93176430adfa8b51604036e5d3d
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-25 14:29:04 -0500

    remove test for login

--- src/presentational/NavBar.js
+++ src/presentational/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = (props) =>  {
           <Container>
             <Link to="/" className="item"><Header inverted as='h3'><Image src={logo} alt="logo"/> Ed Ezekiel</Header></Link>
             <Link to="/about" className="item"><Header inverted as='h3'>About Me</Header></Link>
-            { props.user ? <Link to="/" className="item" onClick={props.logout}> <Header inverted as='h3'>Logout</Header> </Link> : null }
+            <Link to="/" className="item" onClick={props.logout}> <Header inverted as='h3'>Logout</Header> </Link>
             { props.user ? <Link to="/new" className="item"><Header inverted as='h3'>New Article</Header></Link> : null }
             </Container>
         </Menu>
